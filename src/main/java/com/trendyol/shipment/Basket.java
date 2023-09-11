6b03ac4b7da42d9d7860eafb200695796c42bfdd
Deanntu <tugrademirel19@ku.edu.tr>
2023-09-11 14:36:21 +0300

    Solved the task using 2 helper methods.

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,14 @@
 package com.trendyol.shipment;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Basket {
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        return findLargestSize(products);
     }
 
     public List<Product> getProducts() {
@@ -16,5 +17,33 @@ public class Basket {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+    private ShipmentSize findLargestSize(List<Product> products) {
+        Map<ShipmentSize, Long> sizeCount = products.stream()
+                .map(Product::getSize)
+                .collect(Collectors.groupingBy(
+                        size -> size,
+                        Collectors.counting()
+                ));
+
+        for (ShipmentSize size : sizeCount.keySet()) {
+            if (sizeCount.get(size) >= 3) {
+                return getNextSize(size);
+            }
+        }
+
+        return products.stream()
+                .map(Product::getSize)
+                .max(Comparator.naturalOrder())
+                .orElse(null);
+    }
+
+    private static ShipmentSize getNextSize(ShipmentSize size) {
+        return switch (size) {
+            case SMALL -> ShipmentSize.MEDIUM;
+            case MEDIUM -> ShipmentSize.LARGE;
+            case LARGE -> ShipmentSize.X_LARGE;
+            default -> size;
+        };
     }
 }
